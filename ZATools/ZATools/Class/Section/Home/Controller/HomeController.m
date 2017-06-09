9ffb53d6de40f95e6ee92aba9769213819d9e1c9
sunsun <bigsun1992@sina.cn>
2017-06-09 18:04:40 +0800

    123
    123

--- ZATools/ZATools/Class/Section/Home/Controller/HomeController.m
+++ ZATools/ZATools/Class/Section/Home/Controller/HomeController.m
@@ -8,8 +8,10 @@
 
 #import "HomeController.h"
 #import <React/RCTRootView.h>
+#import <WebKit/WKWebView.h>
+#import "WKWebViewJavascriptBridge.h"
 
-@interface HomeController ()
+@interface HomeController () <WKUIDelegate,WKNavigationDelegate>
 
 @end
 
@@ -19,6 +21,8 @@
     [super viewDidLoad];
 
     [self addReactView];
+    
+    
 }
 
 
@@ -27,7 +31,34 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)loadWebView{
+    WKWebViewConfiguration *configuration = [[WKWebViewConfiguration alloc] init];
+    NSSet *dataTypes = [WKWebsiteDataStore allWebsiteDataTypes];
+    [[WKWebsiteDataStore defaultDataStore] removeDataOfTypes:dataTypes modifiedSince:[NSDate dateWithTimeIntervalSince1970:0] completionHandler:^{
+        
+    }];
+    WKWebView *webView = [[WKWebView alloc] initWithFrame:CGRectMake(0, 0, kScreenWidth, kScreenHeight)];
+    [webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://www.baidu.com"] cachePolicy:NSURLRequestReturnCacheDataElseLoad timeoutInterval:20.f]];
+    webView.navigationDelegate = self;
+//    WKWebViewJavascriptBridge *bridge = [WKWebViewJavascriptBridge bridgeForWebView:webView];
+    [self.view addSubview:webView];
+}
+- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation{
+    
+}
 
+- (void)webView:(WKWebView *)webView didFailProvisionalNavigation:(WKNavigation *)navigation{
+    
+}
+
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler{
+    decisionHandler(WKNavigationActionPolicyAllow);
+    
+}
+
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationResponse:(WKNavigationResponse *)navigationResponse decisionHandler:(void (^)(WKNavigationResponsePolicy))decisionHandler{
+    decisionHandler(WKNavigationResponsePolicyAllow);
+}
 
 - (void)addReactView{
     NSURL *jsCodeLocation = [NSURL
